8c30315e28a7184485865d4b8f45e704666cf69b
brianguo <brianguo@tencent.com>
2021-11-03 11:23:18 +0800

    feat: add timeout 2s  file: 08-tcp-connect.go

--- 08-tcp-connect.go
+++ 08-tcp-connect.go
@@ -1,3 +1,4 @@
+
 package main
 
 import (
@@ -7,22 +8,25 @@ import (
 	"time"
 )
 
-func connect_only(addr string) (resp string, err error) {
-	t1 := time.Now()
-	conn, err := net.Dial("tcp", addr)
+
+func connect_only(addr string)  (resp string, err error) {
+    t1 := time.Now()
+    d := net.Dialer{Timeout: time.Second *2 }
+	conn, err := d.Dial("tcp", addr)
 	t2 := time.Now()
 	if err != nil {
-		return fmt.Sprintf("[%s] msg: %s %s %s cost time: %s", t2.Format(time.RFC3339), "connect ", addr, "[failed]", t2.Sub(t1)), err
+		return fmt.Sprintf( "[%s] msg: %s %s %s cost time: %s", t2.Format(time.RFC3339),  "connect ", addr , "[failed]", t2.Sub(t1)), err
 	}
-	defer conn.Close()
-	return fmt.Sprintf("[%s] msg: %s %s %s cost time: %s", t2.Format(time.RFC3339), "connect ", addr, "[success]", t2.Sub(t1)), nil
+	defer conn.Close()	
+	return fmt.Sprintf( "[%s] msg: %s %s %s cost time: %s", t2.Format(time.RFC3339), "connect ", addr , "[success]", t2.Sub(t1)), nil
 }
 
 func main() {
-	if len(os.Args) < 2 {
-		return
-	} else {
-		r, _ := connect_only(os.Args[1])
-		fmt.Println(r)
-	}
+   if (len(os.Args) < 2 ) {
+      return 
+   } else {
+     r, _ := connect_only(os.Args[1])
+     fmt.Println(r)
+   }
 }
+
