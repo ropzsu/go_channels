35a6b74ce3d5bcaa795c30f8f1badfec61d3f4c8
brianguo <brianguo@tencent.com>
2021-11-03 11:16:32 +0800

    add file: 08-tcp-connect.go

--- 08-tcp-connect.go
+++ 08-tcp-connect.go
@@ -1,4 +1,3 @@
-
 package main
 
 import (
@@ -8,25 +7,22 @@ import (
 	"time"
 )
 
-
-func connect_only(addr string)  (resp string, err error) {
-    t1 := time.Now()
+func connect_only(addr string) (resp string, err error) {
+	t1 := time.Now()
 	conn, err := net.Dial("tcp", addr)
 	t2 := time.Now()
 	if err != nil {
-		return fmt.Sprintf( "[%s] msg: %s %s %s cost time: %s", t2.Format(time.RFC3339),  "connect ", addr , "[failed]", t2.Sub(t1)), err
+		return fmt.Sprintf("[%s] msg: %s %s %s cost time: %s", t2.Format(time.RFC3339), "connect ", addr, "[failed]", t2.Sub(t1)), err
 	}
-	defer conn.Close()	
-	return fmt.Sprintf( "[%s] msg: %s %s %s cost time: %s", t2.Format(time.RFC3339), "connect ", addr , "[success]", t2.Sub(t1)), nil
+	defer conn.Close()
+	return fmt.Sprintf("[%s] msg: %s %s %s cost time: %s", t2.Format(time.RFC3339), "connect ", addr, "[success]", t2.Sub(t1)), nil
 }
 
 func main() {
-   if (len(os.Args) < 2 ) {
-      return 
-   } else {
-     r, _ := connect_only(os.Args[1])
-     fmt.Println(r)
-   }
+	if len(os.Args) < 2 {
+		return
+	} else {
+		r, _ := connect_only(os.Args[1])
+		fmt.Println(r)
+	}
 }
-
-
