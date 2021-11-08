290f9635e45261b34bf5a1263921da345e639c31
brianguo <brianguo@tencent.com>
2021-11-08 20:44:28 +0800

    add file: 11-pass_ret_value_by_chan.go

---
+++ 11-pass_ret_value_by_chan.go
@@ -0,0 +1,24 @@
+
+package main 
+
+import "fmt"
+
+func normal_ret(s string) string {
+	return s
+}
+
+func go_channel_ret(s string , ch chan string) {
+	ch <- s 
+}
+
+
+func main () {
+	ch := make(chan string , 1)
+	r1 := normal_ret("hello world")
+	go_channel_ret("hello, go channel", ch)
+
+	fmt.Printf("r1 = \"%s\" \n", r1)
+	fmt.Printf("r2 = \"%s\" \n", <-ch)
+
+}
+
