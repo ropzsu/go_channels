be888cffdce699f236c81441123df7223b1c7b65
brianguo <brianguo@tencent.com>
2021-11-09 10:56:51 +0800

    add file: 14-exec-command.go

--- 14-exec-command.go
+++ 14-exec-command.go
@@ -12,10 +12,11 @@ func main() {
    psCMD := exec.Command("bash" , "-c" , "ps -e -o \"pid,etime,comm,args\" ")
    psOut, _ := psCMD.Output()
 
-   ioutil.WriteFile("ps.log", psOut, 0600)
+   ioutil.WriteFile("/tmp/ps.log", psOut, 0600)
 
-   fmt.Printf("%s \n[%s] msg: %s\n",  string(psOut), time.Now().Format(time.RFC3339),  "Write  ps.log ... done ")
-
+   fmt.Printf("%s \n[%s] msg: %s\n",  string(psOut), 
+              time.Now().Format(time.RFC3339),  
+              "Write  /tmp/ps.log ... done " )        
 }
 
 
