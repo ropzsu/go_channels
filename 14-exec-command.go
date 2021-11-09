c2aefab6d6dad516408dddb98c4e0103894fc9b6
brianguo <brianguo@tencent.com>
2021-11-09 10:54:51 +0800

    add file: 14-exec-command.go

---
+++ 14-exec-command.go
@@ -0,0 +1,21 @@
+
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os/exec"
+	"time"
+)
+
+func main() {
+   psCMD := exec.Command("bash" , "-c" , "ps -e -o \"pid,etime,comm,args\" ")
+   psOut, _ := psCMD.Output()
+
+   ioutil.WriteFile("ps.log", psOut, 0600)
+
+   fmt.Printf("%s \n[%s] msg: %s\n",  string(psOut), time.Now().Format(time.RFC3339),  "Write  ps.log ... done ")
+
+}
+
+
