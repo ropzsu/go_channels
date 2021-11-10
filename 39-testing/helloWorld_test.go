c3e6c04abe8349287d5cd7799713e00b888ec72d
brianguo <brianguo@tencent.com>
2021-11-10 17:23:46 +0800

    add file: 39-testing

--- 39-testing/helloWorld_test.go
+++ 39-testing/helloWorld_test.go
@@ -16,4 +16,30 @@ exit status 1
 FAIL	hello	0.687s
 ZhiwendeMacBook-Pro:39-testing brianguo$
 
+
+
+*/
+
+
+/*
+package morestrings
+
+import "testing"
+
+func TestReverseRunes(t *testing.T) {
+	cases := []struct {
+		in, want string
+	}{
+		{"Hello, world", "dlrow ,olleH"},
+		{"Hello, 世界", "界世 ,olleH"},
+		{"", ""},
+	}
+	for _, c := range cases {
+		got := ReverseRunes(c.in)
+		if got != c.want {
+			t.Errorf("ReverseRunes(%q) == %q, want %q", c.in, got, c.want)
+		}
+	}
+}
+
 */
