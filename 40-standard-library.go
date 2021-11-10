378176a5c424f3769deb37bde1fc152419cc5aa6
brianguo <brianguo@tencent.com>
2021-11-10 17:25:01 +0800

    add file: 40-standard-library.go

---
+++ 40-standard-library.go
@@ -0,0 +1,90 @@
+
+package main
+
+func main() {
+
+}
+/*
+Standard library - pkg.go.dev
+https://pkg.go.dev/std
+
+Directories ¶
+Expand all
+archive
+bufio
+Package bufio implements buffered I/O. It wraps an io.Reader or io.Writer object, creating another object (Reader or Writer) that also implements the interface but provides buffering and some help for textual I/O.
+builtin
+Package builtin provides documentation for Go's predeclared identifiers.
+bytes
+Package bytes implements functions for the manipulation of byte slices.
+compress
+container
+context
+Package context defines the Context type, which carries deadlines, cancellation signals, and other request-scoped values across API boundaries and between processes.
+crypto
+Package crypto collects common cryptographic constants.
+database
+debug
+embed
+Package embed provides access to files embedded in the running Go program.
+encoding
+Package encoding defines interfaces shared by other packages that convert data to and from byte-level and textual representations.
+errors
+Package errors implements functions to manipulate errors.
+expvar
+Package expvar provides a standardized interface to public variables, such as operation counters in servers.
+flag
+Package flag implements command-line flag parsing.
+fmt
+Package fmt implements formatted I/O with functions analogous to C's printf and scanf.
+go
+hash
+Package hash provides interfaces for hash functions.
+html
+Package html provides functions for escaping and unescaping HTML text.
+image
+Package image implements a basic 2-D image library.
+index
+io
+Package io provides basic interfaces to I/O primitives.
+log
+Package log implements a simple logging package.
+math
+Package math provides basic constants and mathematical functions.
+mime
+Package mime implements parts of the MIME spec.
+net
+Package net provides a portable interface for network I/O, including TCP/IP, UDP, domain name resolution, and Unix domain sockets.
+os
+Package os provides a platform-independent interface to operating system functionality.
+path
+Package path implements utility routines for manipulating slash-separated paths.
+plugin
+Package plugin implements loading and symbol resolution of Go plugins.
+reflect
+Package reflect implements run-time reflection, allowing a program to manipulate objects with arbitrary types.
+regexp
+Package regexp implements regular expression search.
+runtime
+Package runtime contains operations that interact with Go's runtime system, such as functions to control goroutines.
+sort
+Package sort provides primitives for sorting slices and user-defined collections.
+strconv
+Package strconv implements conversions to and from string representations of basic data types.
+strings
+Package strings implements simple functions to manipulate UTF-8 encoded strings.
+sync
+Package sync provides basic synchronization primitives such as mutual exclusion locks.
+syscall
+Package syscall contains an interface to the low-level operating system primitives.
+testing
+Package testing provides support for automated testing of Go packages.
+text
+time
+Package time provides functionality for measuring and displaying time.
+unicode
+Package unicode provides data and functions to test some properties of Unicode code points.
+unsafe
+Package unsafe contains operations that step around the type safety of Go programs.
+internal
+*/
