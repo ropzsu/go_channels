52d4e53e36ce51bc39cea5213dc3db9bde40aadb
brianguo <brianguo@tencent.com>
2021-11-12 16:49:37 +0800

    add file: 47-go-keywords.go

---
+++ 47-go-keywords.go
@@ -0,0 +1,29 @@
+package main
+
+func main() { }
+/*
+2.Go只有25个关键字
+
+    break        default      func         interface    select
+    case         defer        go           map          struct
+    chan         else         goto         package      switch
+    const        fallthrough  if           range        type
+    continue     for          import       return       var
+3.Go还有37个保留字
+
+    Constants:    true  false  iota  nil
+
+    Types:    int  int8  int16  int32  int64  
+              uint  uint8  uint16  uint32  uint64  uintptr
+              float32  float64  complex128  complex64
+              bool  byte  rune  string  error
+
+    Functions:   make  len  cap  new  append  copy  close  delete
+                 complex  real  imag
+                 panic  recover
+
+1.1.7. Go语言声明：
+有四种主要声明方式：
+
+    var（声明变量）, const（声明常量）, type（声明类型） ,func（声明函数）。
+*/
