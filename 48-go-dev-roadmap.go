f65644ff4e5750bf12621912dc97db3b1e1236d3
brianguo <brianguo@tencent.com>
2021-11-12 16:56:56 +0800

    add file: 48-go-dev-roadmap.go

---
+++ 48-go-dev-roadmap.go
@@ -0,0 +1,142 @@
+
+package main 
+func main() { }
+
+/*
+
+
+Alikhll/golang-developer-roadmap: Roadmap to becoming a Go developer in 2020
+https://github.com/Alikhll/golang-developer-roadmap
+
+Resources
+Prerequisites
+
+Go
+Go Modules
+SQL
+General Development Skills
+
+Learn GIT, create a few repositories on GitHub, share your code with other people
+Know HTTP(S) protocol, request methods (GET, POST, PUT, PATCH, DELETE, OPTIONS)
+Don't be afraid of using Google, Power Searching with Google
+Read a few books about algorithms and data structures
+Learn about implementation of a basic Authentication
+Solid principles, architectural & design patterns, ecc
+Basics of software testing (unit, integration, e2e)
+CLI Tools
+
+cobra
+urfave/cli
+Web Frameworks + Routers
+
+Beego
+Chi
+Echo
+Fiber
+Gin
+Revel
+Databases
+
+Relational
+SQL Server
+PostgreSQL
+MariaDB
+MySQL
+CockroachDB
+Cloud Databases
+CosmosDB
+DynamoDB
+Search Engines
+ElasticSearch
+Solr
+Sphinx
+NoSQL
+MongoDB
+Redis
+Apache Cassandra
+RavenDB
+CouchDB
+ORMs
+
+Gorm
+Xorm
+Caching
+
+GCache
+Distributed Cache
+Go-Redis
+GoMemcached
+Logging
+
+Log Frameworks
+Zap
+ZeroLog
+Logrus
+Log Management System
+Sentry.io
+Loggly.com
+Distributed Tracing
+Jaeger
+Real-Time Communication
+
+Centrifugo
+Melody
+API Clients
+
+REST
+Gentleman
+GRequests
+heimdall
+GraphQL
+gqlgen
+graphql-go
+Good to Know
+
+Validator
+Glow
+GJson
+Authboss
+Go-Underscore
+Testing
+
+Unit, Behavior, Integration Testing
+GoMock
+Testify
+GinkGo
+GoMega
+GoCheck
+GoDog
+GoConvey
+Dockertest
+E2E Testing
+Selenium
+Endly
+Task Scheduling
+
+Gron
+JobRunner
+Gocron
+MicroServices
+
+Message-Broker
+RabbitMQ
+Apache Kafka
+ActiveMQ
+Azure Service Bus
+Building message-driven
+Watermill
+Message-Bus
+Frameworks
+GoKit
+go-zero
+Micro
+rpcx
+RPC
+Protocol Buffers
+gRPC-Go
+gRPC-Gateway
+Twirp
+Go-Patterns
+
+
+*/
